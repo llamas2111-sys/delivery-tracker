7cd29c2bb87789537faaa86b801d390bc2f6d9b2
llamas2111-sys <llamas2111@gmail.com>
2025-07-28 11:35:00 -0600

    Add files via upload

---
+++ Qwen_bash_20250728_lb8vjpqz2.sh
@@ -0,0 +1,9 @@
+# 1. Crear carpeta del proyecto
+mkdir delivery-tracker
+cd delivery-tracker
+
+# 2. Inicializar proyecto Flutter
+flutter create .
+
+# 3. Abrir en VS Code (opcional, pero recomendado)
+code .
